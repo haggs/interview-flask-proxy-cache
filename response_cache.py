18d17505dc09c0df044245a90e4c7643293566f2
Dan Haggerty <daniel.haggerty@altatececuador.com>
2015-07-24 17:05:48 -0500

    Added a lot of code, should've committed more often. Added a route/template/style for a home page for our server that gives cache info. Added basic ResponseCache structure and logic. Added basic proxy stuff.

--- response_cache.py
+++ response_cache.py
@@ -1,14 +1,34 @@
 '''
 This is our really simple cache class. It's more-or-less a key-value store of URL's to GET responses.
 '''
+from datetime import datetime
+import sys
+
 class ResponseCache:
 
     def __init__(self, cache_duration_ms = 30 * 1000, cache_size_bytes = 1024, cache_size_elements = 100):
-        print "Instantiating a ResponseCache"
-        self.validate_conf(cache_duration_ms, cache_size_bytes, cache_size_elements)
+        self.cache_duration_ms   = cache_duration_ms
+        self.cache_size_bytes    = cache_size_bytes
+        self.cache_size_elements = cache_size_elements
+        self.validate_configuration(cache_duration_ms, cache_size_bytes, cache_size_elements)
         self.cache_dict = {}
 
-    def validate_conf(self, cache_duration_ms, cache_size_bytes, cache_size_elements):
+    def __contains__(self, key):
+        return key in self.cache_dict
+
+    def __getitem__(self, item):
+        return self.cache_dict[item]
+
+    def __iter__(self):
+        return iter(self.cache_dict)
+
+    def length(self):
+        return len(self.cache_dict)
+
+    def size_bytes(self):
+        return 0
+
+    def validate_configuration(self, cache_duration_ms, cache_size_bytes, cache_size_elements):
         if cache_duration_ms <= 0:
             raise Exception("ResponseCache: CACHE_DURATION_MS must be greater than 0")
         if cache_size_bytes <= 0:
@@ -16,8 +36,17 @@ class ResponseCache:
         if cache_size_elements <= 0:
             raise Exception("ResponseCache: CACHE_SIZE_ELEMENTS must be greater than 0")
 
-    def get(self, request=None):
-        if request not in self.cache_dict:
-            self.cache_dict[request] = "REQUEST"
+    def get_time(self, url):
+        return self.cache_dict[url]['last_updated']
+
+    def get_size(self, url):
+        return sys.getsizeof(self.cache_dict[url])
+
+    def insert(self, url, response):
+        if url in self.cache_dict:
+            return self.cache_dict[url]
         else:
-            return self.cache_dict[request]
+            self.cache_dict[url] = {'response': response, "last_updated": datetime.now()}
+
+    def delete(self, url):
+        del self.cache_dict[url]
--- server.py
+++ server.py
@@ -2,15 +2,19 @@
 This is a simple Flask app that acts as a caching proxy for GET requests. It accepts incoming GET
 requests and forwards them on their destination using a simple caching layer.
 '''
-from flask import Flask, render_template, request, abort, Response, redirect
+from flask import Flask, render_template, request, abort, Response, redirect, url_for
 import requests
 from response_cache import ResponseCache
 from urlparse import urlparse
+# import logging
+
+# Setup logging
+# logging.basicConfig(level=logging.INFO)
+# LOG = logging.getLogger("main.py")
 
 
 # Define our Flask app
 app = Flask(__name__)
-
 
 # Load in our conf.py file
 try:
@@ -26,9 +30,61 @@ except:
 # Instantiate a ResponseCache with the attributes given by the configuration file
 CACHE = ResponseCache(CACHE_DURATION_MS, CACHE_SIZE_BYTES, CACHE_SIZE_ELEMENTS)
 
+CACHE.insert("www.google.com", "GOOGLE RESPONSE")
+CACHE.insert("www.facebook.com", "FACEBOOK RESPONSE")
+CACHE.insert("www.reddit.com", "REDDIT RESPONSE")
+
+@app.route('/')
+def home():
+    return render_template('index.html',
+                           port = PORT,
+                           cache_duration_ms = CACHE_DURATION_MS,
+                           cache_size_bytes = CACHE_SIZE_BYTES,
+                           cache_size_elements = CACHE_SIZE_ELEMENTS,
+                           response_cache = CACHE,)
+
+@app.route('/<path:url>')
+def root(url):
+    # LOG.info("Root route, path: %s", url)
+    # If referred from a proxy request, then redirect to a URL with the proxy prefix.
+    # This allows server-relative and protocol-relative URLs to work.
+    proxy_ref = proxy_ref_info(request)
+    if proxy_ref:
+        redirect_url = "/p/%s/%s%s" % (proxy_ref[0], url, ("?" + request.query_string if request.query_string else ""))
+        # LOG.info("Redirecting referred URL to: %s", redirect_url)
+        return redirect(redirect_url)
+    # Otherwise, default behavior
+    return "Welcome to my little cache proxy server"
 
 
-CHUNK_SIZE = 1024
+@app.route('/p/<path:url>')
+def proxy(url):
+    """Fetches the specified URL and streams it out to the client.
+    If the request was referred by the proxy itself (e.g. this is an image fetch for
+    a previously proxied HTML page), then the original Referer is passed."""
+    # if not url in CACHE:
+    #     return CACHE.insert(url)['response']
+    # return CACHE[url]['response']
+
+    r = get_source_rsp(url)
+    # LOG.info("Got %s response from %s",r.status_code, url)
+    headers = dict(r.headers)
+    def generate():
+        for chunk in r.iter_content(1024):
+            yield chunk
+    return Response(generate(), headers = headers)
+
+
+def get_source_rsp(url):
+        url = 'http://%s' % url
+        # LOG.info("Fetching %s", url)
+        # Pass original Referer for subsequent resource requests
+        proxy_ref = proxy_ref_info(request)
+        headers = { "Referer" : "http://%s/%s" % (proxy_ref[0], proxy_ref[1])} if proxy_ref else {}
+        # Fetch the URL, and stream it back
+        # LOG.info("Fetching with headers: %s, %s", url, headers)
+        return requests.get(url, stream=True , params = request.args, headers=headers)
+
 
 def split_url(url):
     """Splits the given URL into a tuple of (protocol, host, uri)"""
@@ -36,6 +92,7 @@ def split_url(url):
     rest = rest[2:].split('/', 1)
     host, uri = (rest[0], rest[1]) if len(rest) == 2 else (rest[0], "")
     return (proto, host, uri)
+
 
 def proxy_ref_info(request):
     """Parses out Referer info indicating the request is from a previously proxied page.
@@ -53,64 +110,11 @@ def proxy_ref_info(request):
         if first in "pd":
             parts = rest.split("/", 1)
             r = (parts[0], parts[1]) if len(parts) == 2 else (parts[0], "")
+            # LOG.info("Referred by proxy host, uri: %s, %s", r[0], r[1])
             return r
     return None
 
-def get_source_rsp(url):
-    url = 'http://%s' % url
-    # Pass original Referer for subsequent resource requests
-    proxy_ref = proxy_ref_info(request)
-    headers = { "Referer" : "http://%s/%s" % (proxy_ref[0], proxy_ref[1])} if proxy_ref else {}
-    # Fetch the URL, and stream it back
-    return requests.get(url, stream=True , params = request.args, headers=headers)
-
-
-
-
-
-
-
-@app.route('/cache/<path:url>')
-def cached_proxy_handler(url):
-    '''
-    If someone goes to 'localhost:5000/cache/http://www.google.com' (for example), use
-    the cache to deliver the response
-    '''
-    print "Someone visited localhost:5000/cache/" + url
-
-    r = get_source_rsp(url)
-    headers = dict(r.headers)
-    def generate():
-        for chunk in r.iter_content(CHUNK_SIZE):
-            yield chunk
-    return Response(generate(), headers = headers)
-
-
-@app.route('/<path:url>')
-def proxy_handler(url):
-    '''
-    If someone goes to 'localhost:5000/http://www.google.com' (for example), forward them
-    to that URL. If they forgot to specify a scheme, add it to the URL before redirecting
-    '''
-    print "Someone visited localhost:5000/" + url
-
-    r = get_source_rsp(url)
-    headers = dict(r.headers)
-    def generate():
-        for chunk in r.iter_content(CHUNK_SIZE):
-            yield chunk
-    return Response(generate(), headers = headers)
-
-
-@app.route('/')
-def root_handler():
-    '''
-    If someone goes to 'localhost:5000/', display a little welcome message for them.
-    '''
-    print "Someone visited localhost:5000"
-    return "Welcome to the Flask Proxy Cache"
-
-
 # Run the server
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=PORT)
+
