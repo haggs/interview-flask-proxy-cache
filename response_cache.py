52f85b897371316ed9110d7e376bc9e9d3674b9c
Dan Haggerty <daniel.haggerty@altatececuador.com>
2015-07-25 00:27:59 -0500

    Basic functionality is all there. Just need to test some more and clean up my code :D

--- response_cache.py
+++ response_cache.py
@@ -1,6 +1,9 @@
-'''
-This is our really simple cache class. It's more-or-less a key-value store of URL's to GET responses.
-'''
+#########################################################################################################
+# This is our really simple cache class. It's more-or-less a key-value store of URL's to GET responses.
+#########################################################################################################
+from flask import request
+import requests
+from urlparse import urlparse
 from datetime import datetime
 import pytz
 import sys
@@ -26,9 +29,6 @@ class ResponseCache:
     def length(self):
         return len(self.cache_dict)
 
-    def size_bytes(self):
-        return 0
-
     def validate_configuration(self, cache_duration_ms, cache_size_bytes, cache_size_elements):
         if cache_duration_ms <= 0:
             raise Exception("ResponseCache: CACHE_DURATION_MS must be greater than 0")
@@ -41,7 +41,7 @@ class ResponseCache:
         return self.cache_dict[url]['last_updated'].strftime("%Y-%m-%d %H:%M:%S %Z")
 
     def get_size(self, url):
-        return sys.getsizeof(self.cache_dict[url])
+        return sys.getsizeof(self.cache_dict[url]['response'])
 
     def get_total_size(self):
         size = 0
@@ -49,12 +49,84 @@ class ResponseCache:
             size += self.get_size(url)
         return size
 
-    def insert(self, url, response):
-        if url in self.cache_dict:
-            return self.cache_dict[url]
-        else:
-            result = self.cache_dict[url] = {'response': response, "last_updated": datetime.now(pytz.timezone("US/Pacific"))}
-            return result
+    def cache_expired(self, url):
+        age_ms = datetime.now(pytz.timezone("US/Pacific")) - self.cache_dict[url]['last_updated']
+        if age_ms.seconds * 1000 + age_ms.microseconds / 1000 > self.cache_duration_ms:
+            return True
+        return False
 
+    def delete_oldest(self):
+        oldest = datetime.now(pytz.timezone("US/Pacific"))
+        for url in self.cache_dict:
+            if self.cache_dict[url]['last_updated'] < oldest:
+                self.delete(url)
+                break
+
+    def insert(self, url):
+        print "Inserting URL:", url
+
+        # If adding this element puts us over our cache size (elements) limit, delete this oldest record
+        if len(self.cache_dict) + 1 > self.cache_size_elements:
+            print "Reached cache element limit, deleting oldest record"
+            self.delete_oldest()
+
+        # Fetch the URL. What we're actually caching is the request content as a list of elements of size 1 KB
+        proxy_ref = self.parse_referer_info(request)
+        headers = { "Referer" : "http://%s/%s" % (proxy_ref[0], proxy_ref[1])} if proxy_ref else {}
+        req = requests.get(url, stream=True , params = request.args, headers=headers)
+        response = list(req.iter_content(1024))
+
+        # If adding this element puts us over our cache size (Bytes) limit, keep deleting the oldest
+        # record until we have space for it
+        while self.get_total_size() + sys.getsizeof(response) > self.cache_size_bytes:
+            print "Reached cache size limit, deleting oldest record(s)"
+            self.delete_oldest()
+
+        # Finally add the response, its headers, and the time we updated the record to the internal cache dictionary.
+        self.cache_dict[url] = { "response" : response,
+                                 "headers"  : dict(req.headers),
+                                 "last_updated" : datetime.now(pytz.timezone("US/Pacific"))
+                               }
+
+    # Delete the URL from the internal cache dictionary
     def delete(self, url):
+        print "Deleting URL:", url
         del self.cache_dict[url]
+
+    # Fetches the specified URL and streams it out to the client.
+    # If the request was referred by the proxy itself (e.g. this is an image fetch for
+    # a previously proxied HTML page), then the original Referer is passed.
+    def get(self, url):
+        url = 'http://' + url
+        # LOG.info("Fetching %s", url)
+        # Pass original Referer for subsequent resource requests
+
+        # Fetch the URL, and stream it back
+        # LOG.info("Fetching with headers: %s, %s", url, headers)
+        if url not in self.cache_dict:
+            print "URL doesn't exist in cache, inserting: " + url
+            self.insert(url)
+
+        elif self.cache_expired(url):
+            print "URL exists in cache but is stale, deleting: " + url
+            self.delete(url)
+            self.insert(url)
+
+        else:
+            print "URL exists in cache and is fresh:" + url
+        return self.cache_dict[url]['headers'], self.cache_dict[url]['response']
+
+
+    def parse_referer_info(self, request):
+        ref = request.headers.get('referer')
+        if ref:
+            uri = urlparse(ref).path
+
+            if uri.find("/") < 0:
+                return None
+            uri_split = uri.split("/", 2)
+            if uri_split[1] in "proxyd":
+                parts = uri_split[2].split("/", 1)
+                r = (parts[0], parts[1]) if len(parts) == 2 else (parts[0], "")
+                return r
+        return None
--- server.py
+++ server.py
@@ -1,8 +1,9 @@
-'''
-This is a simple Flask app that acts as a caching proxy for GET requests. It accepts incoming GET
-requests and forwards them on their destination using a simple caching layer.
-'''
-from flask import Flask, render_template, request, abort, Response, redirect, url_for
+#########################################################################################################
+# This is a simple Flask app that acts as a caching proxy for GET requests. It accepts incoming GET
+# requests and forwards them on their destination using a simple caching layer represented by the
+# ResponseCache class.
+#########################################################################################################
+from flask import Flask, url_for, render_template, request, abort, Response, redirect
 import requests
 from response_cache import ResponseCache
 from urlparse import urlparse
@@ -22,98 +23,92 @@ try:
     assert CACHE_DURATION_MS   and isinstance(CACHE_DURATION_MS, int)
     assert CACHE_SIZE_BYTES    and isinstance(CACHE_SIZE_BYTES, int)
     assert CACHE_SIZE_ELEMENTS and isinstance(CACHE_SIZE_ELEMENTS, int)
+    assert PORT                and isinstance(PORT, int)
 except:
     raise Exception("Import of conf.py was unsuccessful. Make sure you've defined a conf.py containing "
-                    "CACHE_DURATION_MS, CACHE_SIZE_BYTES, CACHE_SIZE_ELEMENTS defined as ints")
+                    "CACHE_DURATION_MS, CACHE_SIZE_BYTES, CACHE_SIZE_ELEMENTS, and PORT defined as ints")
 
 
 # Instantiate a ResponseCache with the attributes given by the configuration file
 CACHE = ResponseCache(CACHE_DURATION_MS, CACHE_SIZE_BYTES, CACHE_SIZE_ELEMENTS)
 
 
-@app.route('/')
+# Display the rendered proxyinfo page if the user navigates to 'localhost:5000/proxyinfo'
+@app.route('/proxyinfo')
 def home():
-    return render_template('index.html',
+    return render_proxyinfo_page()
+
+# Return the rendered proxyinfo.html page
+def render_proxyinfo_page():
+    return render_template('proxyinfo.html',
                            port = PORT,
                            cache_duration_ms = CACHE_DURATION_MS,
                            cache_size_bytes = CACHE_SIZE_BYTES,
                            cache_size_elements = CACHE_SIZE_ELEMENTS,
                            response_cache = CACHE,)
 
-@app.route('/<path:url>')
-def root(url):
-    # LOG.info("Root route, path: %s", url)
-    # If referred from a proxy request, then redirect to a URL with the proxy prefix.
-    # This allows server-relative and protocol-relative URLs to work.
-    # CACHE.insert(url, str(url) + " RESPONSE")
-    proxy_ref = proxy_ref_info(request)
-    if proxy_ref:
-        redirect_url = "/p/%s/%s%s" % (proxy_ref[0], url, ("?" + request.query_string if request.query_string else ""))
-        # LOG.info("Redirecting referred URL to: %s", redirect_url)
-        return redirect(redirect_url)
-    # Otherwise, default behavior
-    return "Welcome to my little cache proxy server"
-
-
+#
 @app.route('/proxy/<path:url>')
 def proxy(url):
-    CACHE.insert(url, str(url) + " RESPONSE")
-    """Fetches the specified URL and streams it out to the client.
-    If the request was referred by the proxy itself (e.g. this is an image fetch for
-    a previously proxied HTML page), then the original Referer is passed."""
-    # if not url in CACHE:
-    #     return CACHE.insert(url)['response']
-    # return CACHE[url]['response']
-
-    r = get_source_rsp(url)
+    headers, request_content = CACHE.get(url)
     # LOG.info("Got %s response from %s",r.status_code, url)
-    headers = dict(r.headers)
     def generate():
-        for chunk in r.iter_content(1024):
+        for chunk in request_content:
             yield chunk
     return Response(generate(), headers = headers)
 
-
-def get_source_rsp(url):
-        url = 'http://%s' % url
-        # LOG.info("Fetching %s", url)
-        # Pass original Referer for subsequent resource requests
-        proxy_ref = proxy_ref_info(request)
-        headers = { "Referer" : "http://%s/%s" % (proxy_ref[0], proxy_ref[1])} if proxy_ref else {}
-        # Fetch the URL, and stream it back
-        # LOG.info("Fetching with headers: %s, %s", url, headers)
-        return requests.get(url, stream=True , params = request.args, headers=headers)
-
-
-def split_url(url):
-    """Splits the given URL into a tuple of (protocol, host, uri)"""
-    proto, rest = url.split(':', 1)
-    rest = rest[2:].split('/', 1)
-    host, uri = (rest[0], rest[1]) if len(rest) == 2 else (rest[0], "")
-    return (proto, host, uri)
+#
+@app.route('/<path:url>')
+def root(url):
+    # CACHE.insert(url, str(url) + " RESPONSE")
+    # LOG.info("Root route, path: %s", url)
+    # If referred from a proxy request, then redirect to a URL with the proxy prefix.
+    # This allows server-relative and protocol-relative URLs to work.
+    proxy_ref = CACHE.parse_referer_info(request)
+    if proxy_ref:
+        redirect_url = "/proxy/%s/%s%s" % (proxy_ref[0], url, ("?" + request.query_string if request.query_string else ""))
+        # LOG.info("Redirecting referred URL to: %s", redirect_url)
+        return redirect(redirect_url)
+    # Otherwise, default behavior
+    return render_proxyinfo_page()
 
 
-def proxy_ref_info(request):
-    """Parses out Referer info indicating the request is from a previously proxied page.
-    For example, if:
-        Referer: http://localhost:8080/p/google.com/search?q=foo
-    then the result is:
-        ("google.com", "search?q=foo")
-    """
-    ref = request.headers.get('referer')
-    if ref:
-        _, _, uri = split_url(ref)
-        if uri.find("/") < 0:
-            return None
-        first, rest = uri.split("/", 1)
-        if first in "pd":
-            parts = rest.split("/", 1)
-            r = (parts[0], parts[1]) if len(parts) == 2 else (parts[0], "")
-            # LOG.info("Referred by proxy host, uri: %s, %s", r[0], r[1])
-            return r
-    return None
+
+
+# # Fetches the specified URL and streams it out to the client.
+# # If the request was referred by the proxy itself (e.g. this is an image fetch for
+# # a previously proxied HTML page), then the original Referer is passed.
+# def get_response(url):
+#     url = 'http://%s' % url
+#     # LOG.info("Fetching %s", url)
+#     # Ensure the URL is approved, else abort
+#     # Pass original Referer for subsequent resource requests
+#     proxy_ref = parse_referer_info(request)
+#     headers = { "Referer" : "http://%s/%s" % (proxy_ref[0], proxy_ref[1])} if proxy_ref else {}
+#     # Fetch the URL, and stream it back
+#     # LOG.info("Fetching with headers: %s, %s", url, headers)
+#     return requests.get(url, stream=True , params = request.args, headers=headers)
+
+# Parses out Referer info indicating the request is from a previously proxied page.
+#     For example, if:
+#         Referer: http://localhost:8080/p/google.com/search?q=foo
+#     then the result is:
+#         ("google.com", "search?q=foo")
+# def parse_referer_info(request):
+#
+#     ref = request.headers.get('referer')
+#     if ref:
+#         uri = urlparse(ref).path
+#
+#         if uri.find("/") < 0:
+#             return None
+#         uri_split = uri.split("/", 2)
+#         if uri_split[1] in "proxyd":
+#             parts = uri_split[2].split("/", 1)
+#             r = (parts[0], parts[1]) if len(parts) == 2 else (parts[0], "")
+#             return r
+#     return None
 
 # Run the server
-if __name__ == '__main__':
-    app.run(debug=True, port=PORT)
+app.run(debug=True, port=PORT)
 
